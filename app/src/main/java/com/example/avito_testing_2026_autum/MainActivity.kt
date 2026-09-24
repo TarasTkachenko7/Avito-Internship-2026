ec884d5fd202b007b71b77d4ffde5210b0dc455a
Taras <fartuch.kek@gmail.com>
2026-09-24 19:52:54 +0300

    feat(core): инициализировал разделение слоёв по фичам и объявил начальные зависимости Koin

--- app/src/main/java/com/example/avito_testing_2026_autum/MainActivity.kt
+++ app/src/main/java/com/example/avito_testing_2026_autum/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.avito_testing_2026_autum.ui.theme.AvitoNotesTasksTheme
@@ -19,29 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AvitoNotesTasksTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Avito Notes & Tasks (Foundation Ready)")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AvitoNotesTasksTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/avito_testing_2026_autum/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.example.avito_testing_2026_autum.di
+
+import org.koin.dsl.module
+
+val coreModule = module {
+
+}
+
+val appModules = listOf(
+    coreModule
+)
