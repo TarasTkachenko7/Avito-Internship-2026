1614a8f3abb87eae5c8754c9efaad865810edde7
Taras <fartuch.kek@gmail.com>
2026-09-25 11:32:57 +0300

    feat(data): настройка базы данных Room, создание сущностей и DAO

---
+++ app/src/main/java/com/example/avito_testing_2026_autum/notes/data/local/NoteEntity.kt
@@ -0,0 +1,14 @@
+package com.example.avito_testing_2026_autum.notes.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    val title: String,
+    val text: String? = null,
+    val imageUri: String? = null,
+    val createdAt: Long
+)
